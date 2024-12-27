5e1695eb1c53fd2305bb4636044f2d452fb1927c
Konstantin Gerasimov <christmasfinn.work@gmail.com>
2024-12-27 17:20:18 +0200

    portadded

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template_string
 from datetime import datetime, timedelta
 
@@ -29,3 +30,5 @@ def days_to_weekend():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
